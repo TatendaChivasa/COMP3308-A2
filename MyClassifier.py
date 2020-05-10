241a34728a33e33016e22691c7676d24e1fe7708
TATENDA CHIVASA <tatenda@TATENDAs-MacBook-Pro.local>
2020-05-10 16:57:30 +1000

    working knn

--- MyClassifier.py
+++ MyClassifier.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 import csv
 import math 
-from statistics import mode
+from statistics import *
 
 def main():
 
@@ -19,8 +19,6 @@ def main():
     if "NN" in algorithm:
         k = algorithm[0]
         kNN(k, training, testing)
-  
-    #WHAT IF DATA IS MISSING
 
 def kNN(k, training, testing): #open files see if i need to extract the first column out 
     testing_file = open(testing, "r" )
@@ -32,57 +30,46 @@ def kNN(k, training, testing): #open files see if i need to extract the first co
         with open(training) as File2:
             for row in csv.reader(File2, delimiter= ','):
                 training_array.append(row) #the length of each entry tells us how many columns there are 
-    # training_array.pop(0) #remove the title column
-
 
     testing_array = []
     if os.path.exists(testing):
         with open(testing) as File:
             for row in csv.reader(File, delimiter= ','):
                 testing_array.append(row) #the length of each entry tells us how many columns there are 
-    # testing_array.pop(0) #remove the title column
-    lengthh = len(testing_array[0]) #shows you where to access the 
-    class_t = testing_array[0][(lengthh-1)] #how to access the class
-    # print("class", class_t)
-    # print(type(class_t))
-    # print(type(testing_array[0][0]))
 
     #classification compare with new value with ever training example
     #calculate the euclidean distance
     for test in testing_array: #one row in the testing dataset
+        e_distance = [] 
         for train in training_array: #go through all the rows in the training dataset
             length = len(test) # train = n+ 1 with class column and test = n should have the same length
             distance = 0.0
-            e_distance = [] 
-            for n in range(0, length-1): # going through all the elements to get the euclidean distance
-                #print(train)
-                #print(test)
+            for n in range(0, length): # going through all the elements to get the euclidean distance
                 d = (float(train[n]) - float(test[n])) ** 2 #square the differnce 
                 distance = distance + d
             
             euclideand = math.sqrt(distance)
-            e_distance.append((euclideand, train[length-1])) #insert the distance and the class in the list 
+            e_distance.append((euclideand, train[length])) #insert the distance and the class in the list 
 
-            #sort the list
+        #sort the list
         e_distance.sort(key=lambda tup: tup[0]) #sort by the euclidean distance
         #get the top k elements 
         k = int(k)
         top_k = e_distance[:k]
-        # print("top k\n", top_k)
-        # sorted_euclidean.clear()
-        # e_distance.clear()
         clist = []
         for i in top_k:
             clist.append(i[1]) #put the classes in a list to get the mode
-        # print("\nclasses\n", clist)
-        most = mode(clist)
-        clist.clear()
-        output_classes.append(most)
+        try:
+            most = mode(clist)
+            clist.clear()
+            output_classes.append(most)
+        except StatisticsError as e:
+            output_classes.append("yes")
+            clist.clear()
+        
     
     for i in output_classes:
         print(i)
-
-
 
 if __name__ == "__main__":
     main()
